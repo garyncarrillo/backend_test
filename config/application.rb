44e9a4210c5637fa34f3c6b25aeec93414e3ba46
Garyn Carrillo Caballero <garync@gmail.com>
2024-04-15 09:35:35 -0500

    Add cors

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,19 @@ module TestGaryn
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    #
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, key: '_nombre_de_tu_aplicacion_session'
+    
+    # CORS configuration
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'  # Be cautious with this in production!
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :patch, :put, :delete, :options, :head]
+      end
+    end
   end
 end
