47ae6af13ca2a94c8aa8993b9086348565935aac
Garyn Carrillo Caballero <garync@gmail.com>
2024-04-15 09:30:17 -0500

    Add product model, controller and db configurations

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,52 @@
+class ProductsController < ApplicationController
+  before_action :set_product_service
+
+  def create
+    product = @product_service.create(product_params)
+    
+    if product
+      render json: product, serializer: ProductSerializer, status: :created
+    else
+      render json: { errors: product.errors.messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    product = @product_service.update_product(params, product_params)
+    
+    if product
+      render json: product, serializer: ProductSerializer, status: 201
+    else
+      render json: { errors: product.errors.messages}, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    begin
+      @product_service.destroy(params)
+      head :no_content
+    rescue => e
+      render json: { errors: {base: I18n.t(:cant_be_deleted)} }, status: :unprocessable_entity
+    end
+  end
+
+  def index
+    products = @product_service.fetch_all()
+    render json: products, each_serializer: ProductSerializer, status: 201
+  end
+
+  def show
+    product = @product_service.fetch_one(params)
+    render json: product, serializer: ProductSerializer, status: 201
+  end
+
+  private
+
+  def set_product_service
+    @product_service = ProductService.new
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :is_active)
+  end
+end
