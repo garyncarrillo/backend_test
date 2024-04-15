11b2cc70822aa8c5a7e7d00b6c5b7c746b061f75
Garyn Carrillo Caballero <garync@gmail.com>
2024-04-15 09:33:45 -0500

    verification to authenticate and exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_user!  
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  private
+
+  def render_not_found(exception)
+    render json: { error: exception.message }, status: :not_found
+  end
 end
