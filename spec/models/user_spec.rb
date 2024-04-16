0c4891befddd602be625047751983725bf2f290c
Garyn Carrillo Caballero <garync@gmail.com>
2024-04-16 16:51:33 -0500

    Add unit test to model product and user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have many products" do
+    user = create(:user)
+    product1 = create(:product, user: user)
+    product2 = create(:product, user: user)
+    
+    expect(user.products.count).to eq(2) # Verifica que el usuario tenga dos productos asociados
+  end
+
+  it "has a valid factory" do
+    user = build(:user)
+    expect(user).to be_valid
+  end
+
+  it "is invalid without an email" do
+    user = build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+
+  it "is invalid with a duplicate email" do
+    create(:user, email: "test@example.com")
+    user = build(:user, email: "test@example.com")
+    expect(user).to_not be_valid
+  end
 end
